288792541b4913dfa763a5d69e42ef8b13638d82
Giannis Lagodimos <62063308+lagodimos@users.noreply.github.com>
2025-04-26 00:52:07 +0300

    Add zed settings

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,3 +1,6 @@
 # Bash
 rm -f /home/$USER/.bashrc
 ln -s /home/$USER/git/dotfiles/.bashrc /home/$USER/.bashrc
+
+rm -f /home/$USER/.var/app/dev.zed.Zed/config/zed/settings.json
+ln -s /home/$USER/git/dotfiles/zed/settings.json /home/$USER/.var/app/dev.zed.Zed/config/zed/settings.json
