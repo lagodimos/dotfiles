86c4e8e57719c099bfcb89c6e530b11402247909
Giannis Lagodimos <62063308+lagodimos@users.noreply.github.com>
2026-02-20 18:30:15 +0200

    Create symlink for .scripts/ in /home/$USER

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -5,5 +5,9 @@ ln -s /home/$USER/git/dotfiles/.profile /home/$USER/.profile
 rm -f /home/$USER/.bashrc
 ln -s /home/$USER/git/dotfiles/.bashrc /home/$USER/.bashrc
 
+# Scripts
+rm -rf /home/$USER/.scripts
+ln -s /home/$USER/git/dotfiles/.scripts /home/$USER/.scripts
+
 rm -f /home/$USER/.var/app/dev.zed.Zed/config/zed/settings.json
 ln -s /home/$USER/git/dotfiles/zed/settings.json /home/$USER/.var/app/dev.zed.Zed/config/zed/settings.json
