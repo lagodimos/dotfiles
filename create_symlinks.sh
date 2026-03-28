520a885cd695580eca657e7e6368ed11f6df5a92
Giannis Lagodimos <62063308+lagodimos@users.noreply.github.com>
2026-03-28 10:57:55 +0200

    Add ashell config

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -9,5 +9,10 @@ ln -s /home/$USER/git/dotfiles/.bashrc /home/$USER/.bashrc
 rm -rf /home/$USER/.scripts
 ln -s /home/$USER/git/dotfiles/.scripts /home/$USER/.scripts
 
+# .config
+mkdir -p $HOME/.config/ashell
+rm -f $HOME/.config/ashell/config.toml
+ln -s $HOME/git/dotfiles/.config/ashell/config.toml $HOME/.config/ashell/config.toml
+
 rm -f /home/$USER/.var/app/dev.zed.Zed/config/zed/settings.json
 ln -s /home/$USER/git/dotfiles/zed/settings.json /home/$USER/.var/app/dev.zed.Zed/config/zed/settings.json
